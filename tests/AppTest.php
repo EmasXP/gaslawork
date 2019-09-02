afce99adb1adb7cfc305ba794cbaced912f5a93e
EmasXP <emasxp@gmail.com>
2019-09-02 15:43:37 +0200

    Writing tests for rendering 404 pages.

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -54,4 +54,66 @@ final class AppTest extends TestCase {
         );
         PHPUnitUtil::callMethod($app, "findAndExecuteRoute", ["Dummycontroller/nonexisting", "GET"]);
     }
+
+    public function testDefaultNotFoundHandler()
+    {
+        $expected = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n<title>404 Not Found</title>\n<h1>Not Found</h1>\n<p>The requested URL <i>uri</i> was not found on the server.</p>";
+        $this->expectOutputString($expected);
+
+        $app = \Gaslawork\App::instance(new Routes);
+        $e = new \Gaslawork\Exception\NotFoundException("uri", "Notfound!");
+        PHPUnitUtil::callMethod($app, "handleNotFoundException", [$e]);
+    }
+
+    public function testDefaultNotFoundHandlerNonExistingRoute()
+    {
+        $expected = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n<title>404 Not Found</title>\n<h1>Not Found</h1>\n<p>The requested URL <i>nonexisting</i> was not found on the server.</p>";
+        $this->expectOutputString($expected);
+
+        $app = \Gaslawork\App::instance(new Routes);
+
+        try
+        {
+            PHPUnitUtil::callMethod($app, "findAndExecuteRoute", ["nonexisting", "GET"]);
+        }
+        catch (\Gaslawork\Exception\NotFoundException $e)
+        {
+            PHPUnitUtil::callMethod($app, "handleNotFoundException", [$e]);
+        }
+    }
+
+    public function testCustomNotFoundHandler()
+    {
+        $expected = "Not found: theuri";
+        $this->expectOutputString($expected);
+
+        $container = (new \Gaslawork\Container)
+            ->set(
+                "notFoundHandler",
+                function($c)
+                {
+                    return function(\Gaslawork\Exception\NotFoundException $e)
+                    {
+                        print "Not found: ".$e->getUri();
+                    };
+                }
+            );
+
+        $app = \Gaslawork\App::instance(new Routes, $container);
+        $e = new \Gaslawork\Exception\NotFoundException("theuri", "Notfound!");
+        PHPUnitUtil::callMethod($app, "handleNotFoundException", [$e]);
+    }
+
+    public function testWorkingRequestResponse()
+    {
+        $this->expectOutputString("World!");
+
+        $app = \Gaslawork\App::instance(
+            (new Routes)
+                ->add(new Route("/:controller/:action", "\Gaslawork\Tests\\"))
+        );
+
+        PHPUnitUtil::callMethod($app, "findAndExecuteRoute", ["Dummycontroller/hello", "GET"]);
+    }
+
 }
--- tests/Dummycontroller.php
+++ tests/Dummycontroller.php
@@ -2,4 +2,11 @@
 
 namespace Gaslawork\Tests;
 
-class Dummycontroller extends \Gaslawork\Controller {}
+class Dummycontroller extends \Gaslawork\Controller {
+
+    public function helloAction()
+    {
+        print "World!";
+    }
+
+}
